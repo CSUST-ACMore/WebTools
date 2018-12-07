fa4cc31537f392f072f143601e2a0d33a70bae57
长岛冰茶、 <1373616097@qq.com>
2018-12-07 22:10:49 +0800

    add one key ac

--- SignUp/signup/admin.py
+++ SignUp/signup/admin.py
@@ -3,8 +3,15 @@ from django.contrib import admin
 from .models import Participant
 
 # Register your models here.
+
+
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'school_id', 'qq_number', 'faculty', 'remark']
+    actions = ['all_ac']
+
+    def all_ac(self, request, queryset):
+        queryset.update(remark=0)
+    all_ac.short_description = "全部AC"
 
 
 admin.site.register(Participant, ParticipantAdmin)
