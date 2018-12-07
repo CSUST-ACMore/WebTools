8d070a9d28c37a76801ec928f9ea414c20b7d760
长岛冰茶、 <1373616097@qq.com>
2018-12-07 14:37:48 +0800

    fix bug

--- SignUp/SignUp/settings.py
+++ SignUp/SignUp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n(y(*4%z*d#t0ho2$cx=ewf&ny!fxd)+!f36gb_8$gv9y3f&ui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,4 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
