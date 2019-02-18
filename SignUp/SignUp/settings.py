d90351d7feb009d1f72683a875f2b9a572f66ca4
长岛冰茶、 <1373616097@qq.com>
2019-02-19 01:08:45 +0800

    update

--- SignUp/SignUp/settings.py
+++ SignUp/SignUp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n(y(*4%z*d#t0ho2$cx=ewf&ny!fxd)+!f36gb_8$gv9y3f&ui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
