2c106fd780d10b4429d7448de273a90dca4a0a6d
长岛冰茶、 <1373616097@qq.com>
2019-02-19 21:19:40 +0800

    fix

--- SignUp/signup/urls.py
+++ SignUp/signup/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     url(r'^register', views.register, name='register'),
     url(r'^list', views.par_list, name='list'),
     url(r'^lottery', views.lottery, name='lottery'),
-    url(r'^', views.index, name='index')
+    url(r'^$', views.index, name='index')
 ]
